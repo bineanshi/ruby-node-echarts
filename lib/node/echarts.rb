2403c26bca1b52a608efbde533c2aa4f696fbebc
wuyingchao <wuyingchao@meizu.com>
2017-11-16 16:17:13 +0800

    initialize project

---
+++ lib/node/echarts.rb
@@ -0,0 +1,7 @@
+require "node/echarts/version"
+
+module Node
+  module Echarts
+    # Your code goes here...
+  end
+end
---
+++ lib/node/echarts/version.rb
@@ -0,0 +1,5 @@
+module Node
+  module Echarts
+    VERSION = "0.1.0"
+  end
+end
