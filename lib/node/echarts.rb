cf226fc2d4d5ace6cfcf625331bb668d5db36976
wuyingchao <wuyingchao@meizu.com>
2017-11-17 14:35:39 +0800

    Node::Echarts generate chart successful

--- lib/node/echarts.rb
+++ lib/node/echarts.rb
@@ -1,7 +1,25 @@
 require "node/echarts/version"
+require "json"
 
 module Node
   module Echarts
-    # Your code goes here...
+    # Generate chart picture
+    #   chart("sample.png", {titile: {text: 'T1'},
+    #   xAxis: ['c'],
+    #   series: [{type: 'bar', data: [100]}]
+    #   },400, 400)
+    def self.chart(path, data, width=400, height=400)
+      cmd_str = "var echarts = require('echarts'); var Canvas = require('canvas'); var fs = require('fs'); #{@theme}; echarts.setCanvasCreator(function () { var canvas = new Canvas(128, 128); return canvas; }); var chart = echarts.init(new Canvas(#{width}, #{height}), 'infographic'); chart.setOption(#{data.to_json.gsub("\"", "'")}); fs.writeFileSync('#{path}', chart.getDom().toBuffer()); process.exit()"
+      `node -e "#{cmd_str}"`
+    end
+
+    # Register echarts theme by file
+    def self.register_theme(path)
+      @theme = File.read(path).
+        gsub(/\/\/.*$/, '').  # remove comment
+        gsub("\n", "").       # remove \n
+        gsub(/\s+/, " ").     # compact blank
+        gsub("\"", "'")       # replace " with '
+    end
   end
 end
--- test/node/echarts_test.rb
+++ test/node/echarts_test.rb
@@ -1,11 +1,81 @@
 require "test_helper"
 
 class Node::EchartsTest < Minitest::Test
+  def setup
+    path = File.expand_path('../', __FILE__)
+    @echart_picture_path = File.join(path, 'chart.png')
+    @echart_theme_picture_path = File.join(path, 'theme_chart.png')
+    @theme_path = File.join(path, 'infographic.js')
+  end
   def test_that_it_has_a_version_number
     refute_nil ::Node::Echarts::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_generate_picture
+    Node::Echarts.chart(@echart_picture_path, {
+      title: {
+        text: '代码提交次数',
+        textStyle: {
+          fontSize: 18,
+          fontWeight: 'lighter',
+          color: '#333'
+        }
+      },
+      tooltop: {
+        trigger: 'axis'
+      },
+      xAxis: {
+        data: ["背锅侠"]
+      },
+      yAxis: {},
+      series: [{
+        type: 'bar',
+        barWidth: 40,
+        itemStyle: {
+          normal: {
+            label: {show: true, position: 'top'}
+          }
+        },
+        data: [100]
+      }]
+    })
+    assert File.exist?(@echart_picture_path), "#{@echart_picture_path} generate failed!"
+  end
+
+  def test_it_generate_picture_with_theme
+    Node::Echarts.register_theme(@theme_path)
+    Node::Echarts.chart(@echart_theme_picture_path, {
+      title: {
+        text: '代码提交次数',
+        textStyle: {
+          fontSize: 18,
+          fontWeight: 'lighter',
+          color: '#333'
+        }
+      },
+      tooltop: {
+        trigger: 'axis'
+      },
+      xAxis: {
+        data: ["背锅侠"]
+      },
+      yAxis: {},
+      series: [{
+        type: 'bar',
+        barWidth: 40,
+        itemStyle: {
+          normal: {
+            label: {show: true, position: 'top'}
+          }
+        },
+        data: [100]
+      }]
+    }, 600, 600)
+    assert File.exist?(@echart_theme_picture_path), "#{@echart_theme_picture_path} generate failed!"
+  end
+
+  def teardown
+    File.delete(@echart_theme_picture_path)
+    File.delete(@echart_picture_path)
   end
 end
