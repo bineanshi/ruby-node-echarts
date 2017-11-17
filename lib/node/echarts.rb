4fc0a958b88712ec5e01c19219c7e5bc26789f9c
wuyingchao <wuyingchao@meizu.com>
2017-11-17 16:03:33 +0800

    add comments

--- lib/node/echarts.rb
+++ lib/node/echarts.rb
@@ -4,16 +4,31 @@ require "json"
 module Node
   module Echarts
     # Generate chart picture
-    #   chart("sample.png", {titile: {text: 'T1'},
-    #   xAxis: ['c'],
-    #   series: [{type: 'bar', data: [100]}]
-    #   },400, 400)
-    def self.chart(path, data, width=400, height=400)
+    #
+    # Example:
+    #  >> chart("/home/foo/sample.png", {titile: {text: 'T1'},
+    #                          xAxis: ['c'],
+    #                          series: [{type: 'bar', data: [100]}]
+    #                          },
+    #           400, 400)
+    #
+    #  Arguments:
+    #    path: (string)
+    #    data: (hash)
+    #    width: integer
+    #    height: integer
+    def                        self.chart(path, data, width=400, height=400)
       cmd_str = "var echarts = require('echarts'); var Canvas = require('canvas'); var fs = require('fs'); #{@theme}; echarts.setCanvasCreator(function () { var canvas = new Canvas(128, 128); return canvas; }); var chart = echarts.init(new Canvas(#{width}, #{height}), 'infographic'); chart.setOption(#{data.to_json.gsub("\"", "'")}); fs.writeFileSync('#{path}', chart.getDom().toBuffer()); process.exit()"
       `node -e "#{cmd_str}"`
     end
 
     # Register echarts theme by file
+    #
+    # Example:
+    #  >> Node::Echarts.register_theme("/home/infographic")
+    #
+    # Arguments:
+    #  path: (string)
     def self.register_theme(path)
       @theme = File.read(path).
         gsub(/\/\/.*$/, '').  # remove comment
